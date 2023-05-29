d2e0fb4063e558bd1dc3aacdee5185912f89e08b
Qndndn <hyojin0510@kaist.ac.kr>
2023-05-29 18:04:55 +0900

    check

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/login";
+import ReservationPage from "./pages/reservation";
+import SelectPage from "./pages/select";
+import SignupPage from "./pages/signup";
+import PageNotFound from "./pages/404";
+import Footer from "./components/footer";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={ <LoginPage/> }/>
+          <Route path="/reservation" element={ <ReservationPage/> }/>
+          <Route path="/select" element={ <SelectPage/> }/>
+          <Route path="/signup" element={ <SignupPage/> }/>
+          <Route path="*" element={ <PageNotFound/> }/>
+        </Routes>
+      </BrowserRouter>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -4,10 +4,7 @@ import "./css/footer.css";
 const Footer = () => {
   return (
     <div className={"footer"}>
-      <div className={"footer-content-wrapper"}>
-        <div className={"footer-logo"}>SPARCS Newbie Seminar</div>
-        <div className={"footer-copyright"}>Powered with ♥️ by night@sparcs.org</div>
-      </div>
+      hyooyh
     </div>
   );
 }
